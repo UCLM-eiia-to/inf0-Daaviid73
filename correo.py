c7b5d68a6fd665c91336331185357e1b5a4ab357
Daaviid73 <72891706+Daaviid73@users.noreply.github.com>
2020-10-15 00:20:36 +0200

    Update correo.py

--- correo.py
+++ correo.py
@@ -1,4 +1,4 @@
 # Define aquí mismo la función my_email sin argumentos que 
 # devuelve tu correo electrónico de la UCLM
-def my_email:
-  return print('david.caballero1@alu.uclm.es')
+def my_email():
+  return 'david.caballero1@alu.uclm.es'
